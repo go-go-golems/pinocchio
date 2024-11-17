5a862fe209fd4a237efdb9ad417367e7e005aa97
Manuel Odendahl <wesen@ruinwesen.com>
2024-11-17 10:21:14 -0500

    :sparkles: Add suffix/prefix flags

--- cmd/pinocchio/cmds/temporizer/temporizer.go
+++ cmd/pinocchio/cmds/temporizer/temporizer.go
@@ -2,13 +2,16 @@ package temporizer
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	"github.com/go-go-golems/glazed/pkg/helpers/files"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 func NewTemporizerCommand() *cobra.Command {
-	var name, prefix string
+	var name, filePrefix, contentPrefix, contentSuffix string
 
 	cmd := &cobra.Command{
 		Use:   "temporizer",
@@ -24,19 +27,47 @@ func NewTemporizerCommand() *cobra.Command {
 				_, _ = fmt.Fprintln(os.Stderr, "Deleted files:", deletedFiles)
 			}
 
-			// Read from stdin and write to a temp file
-			tempFilePath, err := files.CopyReaderToTemporaryFile(prefix, name, os.Stdin)
+			// Create a temporary file
+			tempFile, err := files.CreateTemporaryFile(filePrefix, name)
 			if err != nil {
-				_, _ = fmt.Fprintf(os.Stderr, "Error writing to temporary file: %v\n", err)
+				_, _ = fmt.Fprintf(os.Stderr, "Error creating temporary file: %v\n", err)
+				os.Exit(1)
+			}
+			defer tempFile.Close()
+
+			// Write prefix content if specified
+			if contentPrefix != "" {
+				if !strings.HasSuffix(contentPrefix, "\n") {
+					contentPrefix += "\n"
+				}
+				if _, err := tempFile.WriteString(contentPrefix + "\n"); err != nil {
+					_, _ = fmt.Fprintf(os.Stderr, "Error writing prefix content: %v\n", err)
+					os.Exit(1)
+				}
+			}
+
+			// Copy stdin content
+			if _, err := io.Copy(tempFile, os.Stdin); err != nil {
+				_, _ = fmt.Fprintf(os.Stderr, "Error writing content: %v\n", err)
 				os.Exit(1)
 			}
 
-			fmt.Println(tempFilePath)
+			// Write suffix content if specified
+			if contentSuffix != "" {
+				if _, err := tempFile.WriteString("\n" + contentSuffix); err != nil {
+					_, _ = fmt.Fprintf(os.Stderr, "Error writing suffix content: %v\n", err)
+					os.Exit(1)
+				}
+			}
+
+			fmt.Println(tempFile.Name())
 		},
 	}
 
 	cmd.PersistentFlags().StringVarP(&name, "name", "n", "default", "Name of the temporary file")
-	cmd.PersistentFlags().StringVarP(&prefix, "prefix", "p", "temporizer", "Prefix for the temporary file")
+	cmd.PersistentFlags().StringVarP(&filePrefix, "file-prefix", "p", "temporizer", "Prefix for the temporary file name")
+	cmd.PersistentFlags().StringVar(&contentPrefix, "prefix", "", "Content to prepend to the input")
+	cmd.PersistentFlags().StringVar(&contentSuffix, "suffix", "", "Content to append to the input")
 
 	return cmd
 }
