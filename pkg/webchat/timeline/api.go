275536255ae63a2ef451af77313362792f42bb0b
Manuel Odendahl <wesen@ruinwesen.com>
2026-03-08 17:34:01 -0400

    refactor(webchat): drop unused alias api shims

--- pkg/webchat/timeline/api.go
+++
@@ -1,8 +0,0 @@
-package timeline
-
-import root "github.com/go-go-golems/pinocchio/pkg/webchat"
-
-// Service exposes timeline hydration reads.
-type Service = root.TimelineService
-
-var NewService = root.NewTimelineService
